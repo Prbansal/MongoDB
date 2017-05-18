1729c16962daae81b15c31455dae5dddce1f4db9
pbansal <priyank.bansal@cognizant.com>
2017-05-18 07:58:32 +0000

    Get Post command

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 var Meow = mongoose.model('Meow', {
+ user: String,
  text: String
   });
 
@@ -16,6 +17,23 @@ app.get('/meows', function(req, res, next){
         return res.json(meows);
     });
 });
+
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+
+app.put('/meows/remove', function(req, res, next){	
+var meowId=req.body.meow._id;		
+		Meow.remove({_id: meowId}, function(err){
+			return res.send("Deleted Successfully");
+	});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
